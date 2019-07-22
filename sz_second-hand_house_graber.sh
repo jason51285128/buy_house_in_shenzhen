e25b5147847f90a4d5194b7a012b3de86db1dbf8
zhangchen <zhangchen@onething.net>
2019-07-22 18:53:53 +0800

    修改curl语法错误

--- sz_second-hand_house_graber.sh
+++ sz_second-hand_house_graber.sh
@@ -4,6 +4,7 @@ url=http://zjj.sz.gov.cn/ris/bol/szfdc/EsSource.aspx
 __EVENTTARGET=AspNetPager1
 scriptManager2="updatepanel2|${__EVENTTARGET}"
 __EVENTARGUMENT=
+__LASTFOCUS=
 __VIEWSTATE=
 __VIEWSTATEGENERATOR=
 __VIEWSTATEENCRYPTED=
@@ -12,11 +13,13 @@ tep_name=
 ddlPageCount=
 
 totalPage=`cat total.page`
+newTotalPage=
 totalPageSelector="div.titebox.right span.f14.left"
+houseTableSelector="table.table.ta-c.bor-b-1.table-white"
+tmp=
 
-tmp=`curl -s $url | hxnormalize -x`
-newTotalPage=`echo "$tmp" | hxselect "$totalPageSelector" \
-  | w3m -dump -cols 2000 -T 'text/html' | tr -cd [0-9]`
+updateParameters()
+{
 __VIEWSTATE=`echo "$tmp" | hxselect "#__VIEWSTATE" \
   | hxpipe | grep "Avalue"  | cut -d " " -f3` 
 __VIEWSTATEGENERATOR=`echo "$tmp" | hxselect "#__VIEWSTATEGENERATOR" \
@@ -25,27 +28,40 @@ __EVENTVALIDATION=`echo "$tmp" | hxselect "#__EVENTVALIDATION" \
   | hxpipe | grep "Avalue"  | cut -d " " -f3` 
 ddlPageCount=`echo "$tmp" | hxselect "option[selected]" \
   | hxpipe | grep "Avalue"  | cut -d " " -f3`
+}
 
-echo $newTotalPage 
-echo "__EVENTTARGET=$__EVENTTARGET"
-echo "scriptManager2=$scriptManager2"
-echo "__EVENTARGUMENT=$__EVENTARGUMENT"
-echo "__VIEWSTATE=$__VIEWSTATE"
-echo "__VIEWSTATEGENERATOR=$__VIEWSTATEGENERATOR"
-echo "__VIEWSTATEENCRYPTED=$__VIEWSTATEENCRYPTED"
-echo "__EVENTVALIDATION=$__EVENTVALIDATION"
-echo "tep_name=$tep_name"
-echo "ddlPageCount=$ddlPageCount"
+init()
+{
+  tmp=`curl -s "$url" | hxnormalize -x` 
+  newTotalPage=`echo "$tmp" | hxselect "$totalPageSelector" \
+    | w3m -dump -cols 2000 -T 'text/html' | tr -cd [0-9]`
+  echo  "$tmp" |  hxselect "table.table.ta-c.bor-b-1.table-white" \
+    | w3m -dump -cols 2000 -T 'text/html'
+  updateParameters "$tmp"
+}
 
-curl  -s --data-urlencode __EVENTTARGET="$__EVENTTARGET" \
-         --data-urlencode scriptManager2="$scriptManager2" \
-         -data-urlencode __EVENTARGUMENT="$__EVENTARGUMENT" \
-         --data-urlencode __VIEWSTATE="$__VIEWSTATE" \
-         --data-urlencode __VIEWSTATEGENERATOR="$__VIEWSTATEGENERATOR" \
-         --data-urlencode __VIEWSTATEENCRYPTED="$__VIEWSTATEENCRYPTED" \
-         --data-urlencode __EVENTVALIDATION="$__EVENTVALIDATION" \
-         -data-urlencode tep_name="$tep_name" \
-         --data-urlencode ddlPageCount="$ddlPageCount" "$url"
+init
 
+data="--data-urlencode"
+method="-X POST"
+head="--header Content-Type:application/x-www-form-urlencoded"
+option="-s"
+for i in `seq 2 $newTotalPage`; do
+tmp=`curl $option $head $method   \
+         $data "__EVENTTARGET=$__EVENTTARGET" \
+         $data "scriptManager2=$scriptManager2" \
+         $data "__EVENTARGUMENT=$i" \
+         $data "__LASTFOCUS=$__LASTFOCUS" \
+         $data "__VIEWSTATE=$__VIEWSTATE" \
+         $data "__VIEWSTATEGENERATOR=$__VIEWSTATEGENERATOR" \
+         $data "__VIEWSTATEENCRYPTED=$__VIEWSTATEENCRYPTED" \
+         $data "__EVENTVALIDATION=$__EVENTVALIDATION" \
+         $data "tep_name=$tep_name" \
+         $data "ddlPageCount=$ddlPageCount" "$url" | hxnormalize -x`
+echo  "$tmp" |  hxselect "table.table.ta-c.bor-b-1.table-white" \
+  | w3m -dump -cols 2000 -T 'text/html' | sed -n '2, $p'
+updateParameters "$tmp"
+done
 
+echo $newTotalPage > total.page
 
