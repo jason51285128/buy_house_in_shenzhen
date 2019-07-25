2a8965a3d589d94e633b77377f1f31b0fdf0a24f
zhangchen <zhangchen@onething.net>
2019-07-25 19:24:16 +0800

    修改房源验证脚本

--- sz_second-hand_house_verify.sh
+++ sz_second-hand_house_verify.sh
@@ -5,7 +5,7 @@ __VIEWSTATE=
 __VIEWSTATEGENERATOR=
 __VIEWSTATEENCRYPTED=
 __EVENTVALIDATION=
-txtCode=$1
+txtCode=
 checkCode=
 BtCheck="核对"
 
@@ -13,6 +13,8 @@ cookie="verify.cookie"
 safecodeFile="safecode.jpg"
 safecodeUrl=http://zjj.sz.gov.cn/ris/szfdc/MLS/SafeCode.aspx
 safecode=
+taskOut="verify_db"
+taskLog="house.verify"
 
 updateParameters()
 {
@@ -22,18 +24,29 @@ __VIEWSTATEGENERATOR=`echo "$tmp" | hxselect "#__VIEWSTATEGENERATOR" \
   | hxpipe | grep "Avalue"  | cut -d " " -f3`
 __EVENTVALIDATION=`echo "$tmp" | hxselect "#__EVENTVALIDATION" \
   | hxpipe | grep "Avalue"  | cut -d " " -f3` 
+  curl -b "$cookie" -o "$safecodeFile" -s "$safecodeUrl"
+  if [ "$?" != "0" ]; then
+    return
+  fi
+  safecode=`./baidu_ocr.sh -f $safecodeFile -l 0 | sed '$p' -n |  cut -d "\"" -f2`
+  if [ "$?" != "0" ]; then
+    return
+  fi 
 }
 
 init()
 {
   tmp=`curl -c "$cookie" -s "$url" | hxnormalize -x` 
   updateParameters "$tmp"
-  curl -b "$cookie" -o "$safecodeFile" -s "$safecodeUrl"
-  safecode=`./baidu_ocr.sh -f $safecodeFile -l 0 | sed '$p' -n |  cut -d "\"" -f2`
 }
 
 init
+if [ "$?" != "0" ]; then
+  return
+fi 
 
+grabOut="$1"
+for txtCode in `grep $grabOut "[0-9]\{12\}" | awk '{print $7}'`; do
 data="--data-urlencode"
 method="-X POST"
 head="--header Content-Type:application/x-www-form-urlencoded"
@@ -46,9 +59,14 @@ tmp=`curl $option $head $head1 $method   \
          $data "txtCode=$txtCode" \
          $data "checkCode=$safecode" \
          $data "BtCheck=$BtCheck" "$url" | hxnormalize -x`
+if [ -z "$tmp" ]; then
+echo "1 $txtCode" > "$taskLog"
+exit 1
+fi
 echo  "$tmp" | hxselect "table.table.verify-table.table-white.mb20" \
-  | w3m -dump -cols 2000 -T 'text/html'
+  | w3m -dump -cols 2000 -T 'text/html' > "$taskOut/$txtCode"
 updateParameters "$tmp"
+done
 
 
 
