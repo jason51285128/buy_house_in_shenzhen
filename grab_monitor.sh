17846db9814fb3bd91ddc12736bde3f27136e576
zhangchen <zhangchen@onething.net>
2019-07-26 11:46:59 +0800

    监控简本支持监控验证任务

--- grab_monitor.sh
+++ grab_monitor.sh
@@ -2,8 +2,11 @@
 
 taskName1="sz_second-hand_house_graber.sh"
 taskLog1="total.entry"
-dingding="https://oapi.dingtalk.com/robot/send?access_token=02aea34eb941523a5eb7035f2e97fa978fe345844ff2f3410901d35a5e267161"
+task1RestartMsg="grab task exit unexpectly, restart!"
 
+taskName2="sz_second-hand_house_verify.sh"
+taskLog2="house.verify"
+task2RestartMsg="verify task exit unexpectly, restart!"
 
 internal=30
 while [ 1 ] ; do
@@ -11,14 +14,17 @@ isTask1Alive=`ps -efl | grep "$taskName1" | grep -v grep | wc -l`
 if [ $isTask1Alive -eq 0 ]; then
     exitCode=`sed '$p' -n "$taskLog1" | cut -d " " -f1`
     if [ $exitCode -ne 0 ]; then
-        curl -s  "$dingding" \
-             -H 'Content-Type: application/json' \
-             -d '{"msgtype": "text", 
-                  "text": {
-                   "content": "grab task exit unexpectly, restart!"
-                   }
-                }'
+        ./post_dingding_msg.sh "$task1RestartMsg" 
         nohup ./$taskName1 -c > grab.log 2>&1 & 
+    fi
+fi
+isTask2Alive=`ps -efl | grep "$taskName2" | grep -v grep | wc -l`
+if [ $isTask2Alive -eq 0 ]; then
+    exitCode=`sed '$p' -n "$taskLog2" | cut -d " " -f1`
+    opt=`sed '$p' -n "$taskLog2" | cut -d " " -f3`
+    if [ $exitCode -ne 0 ]; then
+        ./post_dingding_msg.sh "$task2RestartMsg" 
+        nohup ./$taskName2 -c "$opt" > grab.log 2>&1 & 
     fi
 fi
 sleep $internal
