09b41341dddfde3367b889fcf44480db8d20f756
zhangchen <zhangchen@onething.net>
2019-08-09 10:08:00 +0800

    添加数据清洗脚本

---
+++ data_washer.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+if (( $# != 1 )); then
+exit 1
+fi
+
+PWD=$(cd "$(dirname "$0")";pwd)
+srcData=($1)
+lock=3
+exec 3>"$PWD/writelock"
+
+#fuction definition
+graberLeaderDataWash()
+{
+  step1In=`./uuid.sh`
+  flock $lock
+  cp "$graberLeaderOut" "$step1In"
+  flock -u $lock
+
+  #step1: padding phone number
+  exec 4<"$step1In"
+  step1Out=`./uuid.sh`
+  exec 5>"$step1Out"
+  lineindex=0
+  line=
+  while ((1)); do
+    read -u 4
+    if (( $? != 0 )); then
+      break
+    fi
+    ((lineindex++))
+    if (( lineindex % 2 == 1 )); then
+      line=$REPLY
+    else
+      phoneNumber=`echo "$REPLY" | tr -cd [0-9]` 
+      echo -e "$line $phoneNumber" >& 5
+    fi
+  done
+  exec 4>&-
+  exec 5>&-
+  rm -f $step1In
+
+  #step2: conv space to tab
+  step2Out=$step1Out
+  sed -i 's/\s\+/\t/g' "$step1Out"
+
+  #setp3: handle missing field: louceng
+  exec 4<$step2Out
+  step3Out=`./uuid.sh`
+  exec 5>$step3Out
+  while ((1));do
+    read -u 4
+    if (( $? != 0 )); then
+      break
+    fi
+    state="在售"
+    date=`date +%F`
+    line=`echo $REPLY | awk '{if ( NF < 10 ) {gsub($5,$5"\t\\N");gsub($NF,$NF"\t'"$state"'\t'"$date"'")}' 'else {gsub($NF,$NF"\t'"$state"'\t'"$date"'")} print $0 }'`
+    echo "$line" >& 5
+  done
+  exec 4>&-
+  exec 5>&-
+  rm -f $step2Out  
+}
+
+defaultWash()
+{
+  return 0
+}
+#fuction definition end
+
+declare -a lastModify
+tsn=
+tsl=`date +%s`
+sleepInterval=10
+taskInterval=60
+graberLeaderOut="graber_leader.out"
+graberLeaderOutLastTime="graber_leader.last"
+verifierOut="verifier.out"
+verifierOutLastTime="verifier.last"
+locationQuerierOut="location_querier.out"
+locationQuerierLastTime="location_querier.last"
+while ((1)); do
+  tsn=`date +%s`
+  if (( tsn - tsl < taskInterval )); then
+    sleep $sleepInterval
+    continue
+  fi
+  tsl=$tsn
+
+  for(( i = 0; i < ${#srcData[*]}; i++ )); do
+    flock $lock
+    tmp=`stat -c %Y ${srcData[$i]}`
+    flock -u $lock
+    if [ "${lastModify[$i]}" != "$tmp" ]; then
+      case ${srcData[$i]} in
+        "$graberLeaderOut")
+          graberLeaderDataWash
+        ;;
+        "$verifierOut")
+          defaultWash
+        ;;
+        "$locationQuerierOut")
+          defaultWash
+        ;;
+        ?)
+          defaultWash
+        ;;
+      esac
+      lastModify[$i]="$tmp"
+    fi
+  done
+
+done
+
