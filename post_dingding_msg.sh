ba7b1b2afb40e8479f4dd2ae588157a6479505ff
zhangchen <zhangchen@onething.net>
2019-09-04 15:54:27 +0800

    修改发送钉钉消息脚本

--- post_dingding_msg.sh
+++ post_dingding_msg.sh
@@ -3,4 +3,4 @@
 dingding="https://oapi.dingtalk.com/robot/send?access_token=6edf6aab371de1c06eac24c9c2a64e239b1f7de63e62840419c7aac93bc3c0e4"
 curl -s  "$dingding" \
    -H 'Content-Type: application/json' \
-   -d "{\"msgtype\": \"text\", \"text\": {\"content\": \"$1\"}}"
+   -d "{\"msgtype\": \"text\", \"text\": {\"content\": \"$1\"}}" >/dev/null 2>&1
