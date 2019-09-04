64dca57d321eb87fc9aecf81c86a15fdf400f16a
zhangchen <zhangchen@onething.net>
2019-09-04 15:23:22 +0800

    更换发送钉钉消息的URL

--- post_dingding_msg.sh
+++ post_dingding_msg.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-dingding="https://oapi.dingtalk.com/robot/send?access_token=02aea34eb941523a5eb7035f2e97fa978fe345844ff2f3410901d35a5e267161"
+dingding="https://oapi.dingtalk.com/robot/send?access_token=6edf6aab371de1c06eac24c9c2a64e239b1f7de63e62840419c7aac93bc3c0e4"
 curl -s  "$dingding" \
    -H 'Content-Type: application/json' \
    -d "{\"msgtype\": \"text\", \"text\": {\"content\": \"$1\"}}"
