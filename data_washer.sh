6bafd7205766a01dca21efcba20cf259379cfde8
zhangchen <zhangchen@onething.net>
2019-09-04 15:20:24 +0800

    修改数据清洗脚本

--- data_washer.sh
+++ data_washer.sh
@@ -9,11 +9,16 @@ lock=3
 exec 3>"$PWD/writelock"
 
 #fuction definition
-graberLeaderDataWash()
+szSecondHandHouseListDataWash()
 {
+  msg="`./send_ts.sh` start sync second_hand house list database..."
+  echo $msg
+  ./post_dingding_msg.sh "$msg"
+
+  #preprocess...
   step1In=`./uuid.sh`
   flock $lock
-  cp "$graberLeaderOut" "$step1In"
+  cp "$szSecondHandHouseListOrigin" "$step1In"
   flock -u $lock
 
   #step1: padding phone number
@@ -45,82 +50,22 @@ graberLeaderDataWash()
   exec 5>&-
   rm -f $step1In
 
-   #setp2: handle missing field: louceng zhuangtai shouchuriqi
-  exec 4<$step1Out
+  #step2: Deduplication
   step2Out=`./uuid.sh`
-  exec 5>$step2Out
-  while ((1));do
-    read -u 4
-    if (( $? != 0 )); then
-      break
-    fi
-    state="在售"
-    date='\\N'
-    line=`echo "$REPLY" | awk '{if ( NF < 10 ) {$5=$5" \\\N";$NF=$NF" '"$state"' '"$date"'"} else {$NF=$NF" '"$state"' '"$date"'"} print $0 }'`
-    echo "$line" >& 5
-  done
-  exec 4>&-
-  exec 5>&-
-  rm -f $step1Out 
-
-  #step3: conv space to tab
-  step3Out="$step2Out"
-  sed -i 's/\s\+/\t/g' "$step3Out"
-
-  #step4: Deduplication
-  step4Out=`./uuid.sh`
-  touch $step4Out
-  for line in `awk '/[0-9]{12}/ {print "\""$0"\""}' "$step3Out" `; do
-    houseCode=`echo "$line" | awk '{print $7}'`
-    isExist=`grep "$houseCode" "$step4Out"`
-    if [ -z "$isExist" ]; then
-      echo "$line" | cut -d "\"" -f2 >> $step4Out
-    fi
-  done
-  rm -f "$step3Out"
-
-  #step5: compare-looking for add and delete
-  step5Out="graber_leader.sql"
-  cat /dev/null > $step5Out
-  database="sz_house_list"
-  table="second_hand"
-  host="94.191.116.177"
-  port="9224"
-  pw="buyhouse@sz"
-  echo "USE $database;" >> "$step5Out"
-  for line in `awk '/[0-9]{12}/ {print "\""$0"\""}' "$step4Out" `; do
-    houseCode=`echo "$line" | awk '{print $7}'`
-    isExist=`grep "$houseCode" "$graberLeaderOutLastTime"`
-    if [ -z "$isExist" ]; then
-      xiangmumingchen=`echo "$line" | cut -d "\"" | awk '{print $1}'` 
-      hetongliushuihao=`echo "$line" | cut -d "\"" | awk '{print $2}'`
-      qushu=`echo "$line" | cut -d "\"" | awk '{print $3}'`
-      mainjipingfangmi=`echo "$line" | cut -d "\"" | awk '{print $4}'`
-      yongtu=`echo "$line" | cut -d "\"" | awk '{print $5}'`
-      louceng=`echo "$line" | cut -d "\"" | awk '{print $6}'`
-      fangyuanbianma=`echo "$line" | cut -d "\"" | awk '{print $7}'`
-      dailizhongjie=`echo "$line" | cut -d "\"" | awk '{print $8}'`
-      faburiqi=`echo "$line" | cut -d "\"" | awk '{print $9}'`
-      lianxidianhua=`echo "$line" | cut -d "\"" | awk '{print $10}'`
-      zhuangtai=`echo "$line" | cut -d "\"" | awk '{print $11}'`
-      shouchuriqi=`echo "$line" | cut -d "\"" | awk '{print $12}'`
-      echo  "INSERT INTO $table VALUES (\'$xiangmumingchen\', \'$hetongliushuihao\', \'$qushu\', \'$mainjipingfangmi\', \'$yongtu\', \'$louceng\', \'$fangyuanbianma\', \'$dailizhongjie\', \'$faburiqi\', \'$zhuangtai\', \'$shouchuriqi\');" >> "$step5Out"
-    fi
-  done
-  date=`./send_ts.sh | cut -d " " -f1`
-  for line in `awk '/[0-9]{12}/ {print "\""$0"\""}' "$graberLeaderOutLastTime" `; do
-    houseCode=`echo "$line" | awk '{print $7}'`
-    isExist=`grep "$houseCode" "$step4Out"`
-    if [ -z "$isExist" ]; then
-      fangyuanbianma=`echo "$line" | cut -d "\"" | awk '{print $7}'`
-      zhuangtai='已售'
-      shouchuriqi="$date"
-      echo  "UPDATE $table SET zhuangtai=$zhuangtai, shouchuriqi=$shouchuriqi WHERE fangyuanbianma=$fangyuanbianma;" >> "$step5Out"
-    fi
-  done
-  mysql -h$host -P$port -uroot -p$pw -e "$step5Out"
-  rm -f $step5Out
-  mv "$step4Out" $step5Out
+  tmp=`./uuid.sh`
+  awk '!a[$0]++' "$step1Out" > "$tmp"
+  awk '!a[$2]++' "$tmp" > "$step2Out" 
+  rm -f "$step1Out" "$tmp"
+  
+  #step3 process diff
+  ./diff_process.sh "$step2Out"  
+  if (( $? != 0 )); then
+    msg="`./send_ts.sh` sync second_hand house list database failed!"
+  else
+    msg="`./send_ts.sh` sync second_hand house list database success!"
+  fi
+  echo "$msg"
+  ./post_dingding_msg.sh "$msg"
 }
 
 defaultWash()
@@ -134,12 +79,7 @@ tsn=
 tsl=`date +%s`
 sleepInterval=10
 taskInterval=60
-graberLeaderOut="graber_leader.out"
-graberLeaderOutLastTime="graber_leader.last"
-verifierOut="verifier.out"
-verifierOutLastTime="verifier.last"
-locationQuerierOut="location_querier.out"
-locationQuerierLastTime="location_querier.last"
+szSecondHandHouseListOrigin="sz_second-hand_house_list.origin"
 while ((1)); do
   tsn=`date +%s`
   if (( tsn - tsl < taskInterval )); then
@@ -154,14 +94,8 @@ while ((1)); do
     flock -u $lock
     if [ "${lastModify[$i]}" != "$tmp" ]; then
       case ${srcData[$i]} in
-        "$graberLeaderOut")
-          graberLeaderDataWash
-        ;;
-        "$verifierOut")
-          defaultWash
-        ;;
-        "$locationQuerierOut")
-          defaultWash
+        "$szSecondHandHouseListOrigin")
+          szSecondHandHouseListDataWash
         ;;
         ?)
           defaultWash
@@ -170,6 +104,4 @@ while ((1)); do
       lastModify[$i]="$tmp"
     fi
   done
-
 done
-
---
+++ diff_process.sh
@@ -0,0 +1,127 @@
+#!/bin/bash
+
+PWD="$(cd "$(dirname "$0")"; pwd)"
+
+beforeExit()
+{
+  rm -f "$tmp" "$newData" "$out" "$oldData" "$diffData"
+}
+
+if (( $# != 1 )); then
+  echo "invalid parameter!"
+  exit 1
+fi 
+
+newOriginData="$1"
+tmp=`./uuid.sh`
+awk '{print $2}' "$newOriginData" > "$tmp"
+newData=`./uuid.sh`
+sort "$tmp" > "$newData"
+
+out=`./uuid.sh`.sql
+exec 3>"$out"
+
+mysqlConf=(`cat mysql.conf`)
+if (( ${#mysqlConf[*]} != 4 )); then
+  echo "invalid mysql config!"
+  exit 1
+fi
+mysql -h${mysqlConf[0]} -u${mysqlConf[1]} -P${mysqlConf[2]}  -p${mysqlConf[3]} \
+  -e "use sz_house_list; select hetongliushuihao from second_hand;" > "$tmp"
+if (( $? != 0 )); then
+  echo "access mysql failed!"
+  exit 1
+fi
+sed -i '1d' "$tmp"
+oldData=`./uuid.sh`
+sort "$tmp" > "$oldData"
+
+diffData=`./uuid.sh`
+diff "$oldData" "$newData" > "$diffData"
+exec 4<"$diffData"
+sold="已售"
+onsell="在售"
+date=`date +%F`
+counter=0
+while ((1)); do
+  read -u 4
+  if (( $? != 0 )); then
+    break
+  fi
+  line=($REPLY)
+  case ${line[0]} in
+  "<")
+    sql="update second_hand set zhuangtai=\"$sold\", shouchuriqi=\"$date\" where hetongliushuihao=\"${line[1]}\";"
+    echo "$sql" >& 3 
+    ;;
+  ">")
+    entry=(`awk "/${line[0]}/" $newOriginData`)
+    if (( ${#entry[*]} < 9 )); then
+      echo "parse diff failed!"
+      exit
+    fi
+    xiangmumingchen=${entry[0]}
+    hetongliushuihao=${entry[1]}
+    qushu=${entry[2]}
+    mianjipingfangmi=${entry[3]}
+    yongtu=${entry[4]}
+    if (( ${#entry[*]} >= 10 )); then
+      louceng=${entry[5]}
+      fangyuanbianma=${entry[6]}
+      dailizhongjie=${entry[7]}
+      faburiqi=${entry[8]}
+      lianxidianhua=${entry[9]}
+    else
+      louceng="\\N"
+      fangyuanbianma=${entry[5]}
+      dailizhongjie=${entry[6]}
+      faburiqi=${entry[7]}
+      lianxidianhua=${entry[8]}
+    fi
+    jiagewan="\\N"
+    weizhi="\\N"
+    counter=0
+    while (( counter < 3 )); do
+      price=(`./sz_second-hand_house_price.sh $fangyuanbianma`)
+      if (( ${#price[*]} < 2 || ${price[0]} != 0 )); then
+        ((counter++))
+        continue
+      fi
+      jiagewan=${price[1]}
+    done
+    counter=0
+    while (( counter < 3 )); do
+      location=(`./query_location.sh "$xiangmumingchen"`) 
+      if (( ${#location[*]} < 2 || ${location[0]} != 0 )); then
+        ((counter++))
+        continue
+      fi
+      weizhi=${location[1]}
+    done
+    zhuangtai="$onsell"
+    shouchuriqi="\\N"
+    sql="insert into second_hand \
+        (xiangmumingchen, hetongliushuihao, qushu, mianjipingfangmi, yongtu, \
+        louceng, fangyuanbianma, dailizhongjie, faburiqi, lianxidianhua, \
+        jiagewan, weizhi, zhuangtai, shouchuriqi) \
+        values
+        (\"$xiangmumingchen\", \"$hetongliushuihao\", \"$qushu\", $mianjipingfangmi, \"$yongtu\", \
+        $louceng, \"$fangyuanbianma\", \"$dailizhongjie\", \"$faburiqi\", $lianxidianhua, \
+        $jiagewan, $weizhi, \"$zhuangtai\", \"$shouchuriqi\");"
+    echo "$sql" >& 3 
+  ;;
+  *)
+    echo "$REPLY" >&3
+  ;;
+  esac
+done
+
+exec 3>&-
+mysql -h${mysqlConf[0]} -u${mysqlConf[1]} -P${mysqlConf[2]}  -p${mysqlConf[3]} \
+  < "$out"
+if (( $? != 0 )); then
+  echo "access mysql failed!"
+  exit 1
+fi
+beforeExit
+exit 0
