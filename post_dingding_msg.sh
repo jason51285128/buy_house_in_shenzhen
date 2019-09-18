7ee9faf8a7753918b908c3af19bec8ab07e6bc98
zhangchen <zhangchen@onething.net>
2019-09-18 10:22:01 +0800

    重置钉钉消息url

--- post_dingding_msg.sh
+++ post_dingding_msg.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-dingding="https://oapi.dingtalk.com/robot/send?access_token=6edf6aab371de1c06eac24c9c2a64e239b1f7de63e62840419c7aac93bc3c0e4"
+dingding="https://oapi.dingtalk.com/robot/send?access_token=a50c97b545405e06399feed5edbcc68743c24ce0c58e4a60fc9a68399ffc3d7d"
 curl -s  "$dingding" \
    -H 'Content-Type: application/json' \
    -d "{\"msgtype\": \"text\", \"text\": {\"content\": \"$1\"}}" >/dev/null 2>&1
