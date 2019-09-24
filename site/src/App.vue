778215f3e3d57a00b2103556bd83d76dd20bad43
zhangchen <zhangchen@onething.net>
2019-09-24 20:14:07 +0800

    修改页面

--- site/src/App.vue
+++ site/src/App.vue
@@ -20,48 +20,26 @@
             </el-col>
           </el-form-item>
           <el-form-item label="价格（万）">
-            <el-col :span="16">
-              <el-radio-group v-model="form.jiagel" style="width:100%">
-                <el-radio-button label>全部</el-radio-button>
-                <el-radio-button label="0-200">200万以下</el-radio-button>
-                <el-radio-button label="200-400">200-400万</el-radio-button>
-                <el-radio-button label="400-500">400-500万</el-radio-button>
-                <el-radio-button label="500-600">500-600万</el-radio-button>
-                <el-radio-button label="600-1000">600-1000万</el-radio-button>
-                <el-radio-button label="1000-">1000万以上</el-radio-button>
-              </el-radio-group>
-            </el-col>
-            <el-col :span="8">
-              <el-col :span="11">
-                <el-input v-model="form.jiagel" placeholder="最低"></el-input>
-              </el-col>
-              <el-col :span="2">-</el-col>
-              <el-col :span="11">
-                <el-input v-model="form.jiageh" placeholder="最高"></el-input>
-              </el-col>
-            </el-col>
+            <el-radio-group v-model="form.jiage">
+              <el-radio-button label>全部</el-radio-button>
+              <el-radio-button label="0-200">200万以下</el-radio-button>
+              <el-radio-button label="200-400">200-400万</el-radio-button>
+              <el-radio-button label="400-500">400-500万</el-radio-button>
+              <el-radio-button label="500-600">500-600万</el-radio-button>
+              <el-radio-button label="600-1000">600-1000万</el-radio-button>
+              <el-radio-button label="1000-">1000万以上</el-radio-button>
+            </el-radio-group>
           </el-form-item>
           <el-form-item label="面积（m²）">
-            <el-col :span="16">
-              <el-radio-group v-model="form.mianjil">
-                <el-radio-button label>全部</el-radio-button>
-                <el-radio-button label="0-70">70m²以下</el-radio-button>
-                <el-radio-button label="70-90">70-90m²</el-radio-button>
-                <el-radio-button label="90-110">90-110m²</el-radio-button>
-                <el-radio-button label="110-130">110-130m²</el-radio-button>
-                <el-radio-button label="130-160">130-160m²</el-radio-button>
-                <el-radio-button label="160-">160m²以上</el-radio-button>
-              </el-radio-group>
-            </el-col>
-            <el-col :span="8">
-              <el-col :span="11">
-                <el-input v-model="form.mianjil" placeholder="最小"></el-input>
-              </el-col>
-              <el-col :span="2">-</el-col>
-              <el-col :span="11">
-                <el-input v-model="form.mianjih" placeholder="最大"></el-input>
-              </el-col>
-            </el-col>
+            <el-radio-group v-model="form.mianji">
+              <el-radio-button label>全部</el-radio-button>
+              <el-radio-button label="0-70">70m²以下</el-radio-button>
+              <el-radio-button label="70-90">70-90m²</el-radio-button>
+              <el-radio-button label="90-110">90-110m²</el-radio-button>
+              <el-radio-button label="110-130">110-130m²</el-radio-button>
+              <el-radio-button label="130-160">130-160m²</el-radio-button>
+              <el-radio-button label="160-">160m²以上</el-radio-button>
+            </el-radio-group>
           </el-form-item>
           <el-form-item label="类型">
             <el-radio-group v-model="form.leixing">
@@ -88,82 +66,56 @@
               <el-input v-model="form.fangyuanbianma" placeholder="房源编码"></el-input>
             </el-col>
             <el-col :span="3">
-              <el-select v-model="form.faburiqi" placeholder="发布日期">
+              <el-select v-model="form.faburiqi" clearable placeholder="发布日期（全部）">
                 <el-option label="一个月内" value="30"></el-option>
                 <el-option label="三个月内" value="90"></el-option>
                 <el-option label="半年内" value="180"></el-option>
               </el-select>
             </el-col>
             <el-col :span="3">
-              <el-select v-model="form.zhuangtai" placeholder="状态">
+              <el-select v-model="form.zhuangtai" clearable placeholder="状态（全部）">
                 <el-option label="在售" value="在售"></el-option>
                 <el-option label="已售" value="已售"></el-option>
               </el-select>
             </el-col>
             <el-col :span="6">
-              <el-select v-model="form.orderby" placeholder="排序">
+              <el-select v-model="form.orderby" clearable placeholder="排序（发布日期由近到远）">
                 <el-option label="按价格从高到低" value="jiagewan desc"></el-option>
                 <el-option label="按价格从低到高" value="jiagewan"></el-option>
                 <el-option label="按发布日期由近到远" value="faburiqi desc"></el-option>
                 <el-option label="按发布日期由远到近" value="faburiqi"></el-option>
               </el-select>
             </el-col>
-            <el-col :span="3">
-              <el-button type="primary" @click="onSubmit" v-loading.fullscreen.lock="loading">查询</el-button>
-            </el-col>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="onSubmit" v-loading.fullscreen.lock="loading">查询</el-button>
           </el-form-item>
         </el-form>
       </el-row>
       <el-row class="tableContainer">
         <el-table :data="tableData">
-          <el-col :span="3">
-            <el-table-column prop="xiangmumingchen" label="项目名称"></el-table-column>
-          </el-col>
-          <el-col :span="3">
-            <el-table-column prop="hetongliushuihao" label="合同流水号"></el-table-column>
-          </el-col>
-          <el-col :span="1">
-            <el-table-column prop="qushu" label="区属"></el-table-column>
-          </el-col>
-          <el-col :span="1">
-            <el-table-column prop="mianjipingfangmi" label="面积(㎡)"></el-table-column>
-          </el-col>
-          <el-col :span="1">
-            <el-table-column prop="yongtu" label="用途"></el-table-column>
-          </el-col>
-          <el-col :span="1">
-            <el-table-column prop="louceng" label="楼层"></el-table-column>
-          </el-col>
-          <el-col :span="3">
-            <el-table-column prop="fangyuanbianma" label="房源编码"></el-table-column>
-          </el-col>
-          <el-col :span="2">
-            <el-table-column prop="jiagewan" label="价格（万）"></el-table-column>
-          </el-col>
-          <el-col :span="2">
-            <el-table-column prop="dailizhongjie" label="代理中介"></el-table-column>
-          </el-col>
-          <el-col :span="2">
-            <el-table-column prop="faburiqi" label="发布日期"></el-table-column>
-          </el-col>
-          <el-col :span="2">
-            <el-table-column prop="lianxidianhua" label="联系电话"></el-table-column>
-          </el-col>
-          <el-col :span="1">
-            <el-table-column prop="zhuangtai" label="状态"></el-table-column>
-          </el-col>
-          <el-col :span="2">
-            <el-table-column prop="shouchuriqi" label="售出日期"></el-table-column>
-          </el-col>
+          <el-table-column prop="xiangmumingchen" label="项目名称"></el-table-column>
+          <el-table-column prop="hetongliushuihao" label="合同流水号"></el-table-column>
+          <el-table-column prop="qushu" label="区属"></el-table-column>
+          <el-table-column prop="mianjipingfangmi" label="面积(㎡)"></el-table-column>
+          <el-table-column prop="yongtu" label="用途"></el-table-column>
+          <el-table-column prop="louceng" label="楼层"></el-table-column>
+          <el-table-column prop="fangyuanbianma" label="房源编码"></el-table-column>
+          <el-table-column prop="jiagewan" label="价格（万）"></el-table-column>
+          <el-table-column prop="dailizhongjie" label="代理中介"></el-table-column>
+          <el-table-column prop="faburiqi" label="发布日期"></el-table-column>
+          <el-table-column prop="lianxidianhua" label="联系电话"></el-table-column>
+          <el-table-column prop="zhuangtai" label="状态"></el-table-column>
+          <el-table-column prop="shouchuriqi" label="售出日期"></el-table-column>
         </el-table>
       </el-row>
       <el-row class="paginationContainer">
         <el-pagination
           @size-change="handleSizeChange"
           @current-change="handleCurrentChange"
-          :current-page="currentPage"
+          :current-page="form.currentPage"
           :page-sizes="pageSizes"
-          :page-size="pageSize"
+          :page-size="form.pageSize"
           layout="total, sizes, prev, pager, next, jumper"
           :total="total"
         ></el-pagination>
@@ -182,20 +134,17 @@ export default {
         xiangmumingchen: "",
         qushu: "",
         leixing: "",
-        jiagel: "",
-        jiageh: "",
-        mianjil: "",
-        mianjih: "",
+        jiage: "",
+        mianji: "",
         fangyuanbianma: "",
         faburiqi: "",
         zhuangtai: "",
-        orderby: ""
+        orderby: "",
+        currentPage: 1,
+        pageSize: 20
       },
       loading: false,
-      originData: [],
       tableData: [],
-      currentPage: 1,
-      pageSize: 20,
       pageSizes: [20, 50, 100, 200],
       total: 0,
       dataUrl: "http://94.191.116.177:15687"
@@ -203,38 +152,32 @@ export default {
   },
   methods: {
     onSubmit() {
+      this.pullData();
+    },
+    handleSizeChange(val) {
+      this.form.pageSize = val;
+      this.pullData();
+    },
+    handleCurrentChange(val) {
+      this.form.currentPage = val;
+      this.pullData();
+    },
+    pullData() {
       this.loading = true;
       axios
         .get(this.dataUrl, { params: this.form })
         .then(res => {
-          this.originData = res.data.tableData;
-          this.total = this.originData.length;
-          this.currentPage = 1;
-          this.tableData = this.originData.slice(
-            0,
-            this.currentPage * this.pageSize
-          );
+          this.tableData = res.data.tableData;
+          this.total = res.data.total;
           this.loading = false;
         })
         .catch(err => {
           console.error(err);
         });
-    },
-    handleSizeChange(val) {
-      this.currentPage = 1;
-      this.pageSize = val;
-      this.tableData = this.originData.slice(
-        0,
-        this.currentPage * this.pageSize
-      );
-    },
-    handleCurrentChange(val) {
-      this.currentPage = val;
-      this.tableData = this.originData.slice(
-        (this.currentPage - 1) * this.pageSize,
-        this.currentPage * this.pageSize
-      );
     }
+  },
+  mounted: function() {
+    this.pullData();
   }
 };
 </script>
@@ -248,6 +191,10 @@ export default {
   margin-top: 20px;
   margin-left: 20%;
   margin-right: 20%;
+  text-align: center;
+}
+
+.line-through {
   text-align: center;
 }
 </style>
